f068e410381679b9631d8aa73ffa317a7e8ca721
Matias-7 <matias.mardones@genstudents.org>
2024-09-11 17:26:01 -0300

    contacto listo

---
+++ script.js
@@ -0,0 +1,10 @@
+function enviarFormulario() {
+    alert('Gracias por tu mensaje. En las próximas 24 horas te contactaremos.');
+ 
+    document.getElementById('nombre').value = '';
+    document.getElementById('apellido').value = '';
+    document.getElementById('correo').value = '';
+    document.getElementById('mensaje').value = '';
+}
+ 
+ 
